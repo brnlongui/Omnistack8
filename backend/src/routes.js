68ba4a24a1bd42331ca8d484a9f1758dbf864b10
brnlongui1 <brunorlongui@gmail.com>
2020-02-27 16:53:57 -0400

    add cors and routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const DevController = require('./controllers/DevController');
 const LikeController = require('./controllers/LikeController');
+const DislikeController = require('./controllers/DislikeController');
 const routes = express.Router();
 
 routes.post('/devs', DevController.store);
-routes.post('/devs/:devsId/likes', LikeController.store)
+routes.get('/devs',DevController.index);
+routes.post('/devs/:devId/likes', LikeController.store);
+routes.post('/devs/:devId/dislikes', DislikeController.store);
 
 module.exports = routes;
